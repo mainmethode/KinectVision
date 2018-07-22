ad79adfd23dfa34fdb92c0bec45a1fba9b515931
krause <justin.krause@rwth-aachen.de>
2018-07-23 01:00:23 +0200

    -added model for robot 3d

---
+++ src/main/java/de/rwth/i5/kinectvision/machinevision/model/RobotModel.java
@@ -0,0 +1,27 @@
+package de.rwth.i5.kinectvision.machinevision.model;
+
+import javafx.geometry.BoundingBox;
+import javafx.geometry.Point3D;
+
+/**
+ * This class is a representation of the robot's current position in the three-dimensional space of the Kinect.
+ * For every movable part which receives its own positioning information by the robot there is a single object.
+ */
+public class RobotModel {
+
+    //TODO: For the beginning we assume that the robot only has one moving arm part and that's it. The bounding
+
+    /**
+     * The bounding boxes represent the bounds of the arms.
+     **/
+    private BoundingBox arm;
+
+    /**
+     *
+     */
+    private Point3D start;
+
+    public void x() {
+
+    }
+}
